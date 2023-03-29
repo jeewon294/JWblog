fd3a8b92cb4f21a135856dbe6cd974fa906caae8
dogby <dogby@DESKTOP-407VL6U>
2023-03-29 19:30:30 +0900

    2023-03-29 : 다시시작

---
+++ JBblog/src/main/java/com/ssamz/jblog/JBblogApplication.java
@@ -0,0 +1,13 @@
+package com.ssamz.jblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBblogApplication.class, args);
+	}
+
+}
