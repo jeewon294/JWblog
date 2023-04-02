974bdd8f9e55c7093a9765c21bfb6915a5f4a99f
dogby <dogby@DESKTOP-407VL6U>
2023-04-02 19:30:00 +0900

    2023-04-02 : 예외처리 및 예외처리 핸들러

--- JBblog/src/main/java/com/ssamz/jblog/controller/UserController.java
+++ JBblog/src/main/java/com/ssamz/jblog/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ssamz.jblog.controller;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssamz.jblog.domain.RoleType;
 import com.ssamz.jblog.domain.User;
+import com.ssamz.jblog.exception.JBlogException;
 import com.ssamz.jblog.persistence.UserRepository;
 
 @Controller
@@ -26,9 +29,16 @@ public class UserController {
 	
 	@GetMapping("/user/get/{id}")
 	public @ResponseBody User getUser(@PathVariable int id) {
-		//특정 id(회원번호)에 해당하는 User 객체 변환
-		User findUser = userRepository.findById(id).get();
+		// 특정 id(회원번호)에 해당하는 User 객체 변환
+		// 검색된 회원이 없을 경우 예외를 리턴하여 처리한다
+		User findUser = userRepository.findById(id).orElseThrow(()->{ // 추상 메소드가 하나만 존재하는 인터페이스는 람다식으로 간단하게 표현 할 수 있다 
+			return new JBlogException(id + "번 회원이 없습니다.");
+		  //userRepository.findById(id).orElseThrow(new Supplier<JBlogException>() {
+		  //@Override
+		  //public JBlogException get() {
+			  //return new JBlogException(id + "번 회원이 없습니다.");
+			//}
+		});
 		return findUser;
 	}
-	
 }
