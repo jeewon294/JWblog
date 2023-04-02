2dc6b2ca18dff58618ecdc5f59ac6dab7874431e
dogby <dogby@DESKTOP-407VL6U>
2023-04-02 19:04:06 +0900

    2023-04-02 : controller 조회 기능 추가

--- JBblog/src/main/java/com/ssamz/jblog/controller/UserController.java
+++ JBblog/src/main/java/com/ssamz/jblog/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ssamz.jblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,5 +24,11 @@ public class UserController {
 		return user.getUsername() + "회원가입 성공";
 	}
 	
+	@GetMapping("/user/get/{id}")
+	public @ResponseBody User getUser(@PathVariable int id) {
+		//특정 id(회원번호)에 해당하는 User 객체 변환
+		User findUser = userRepository.findById(id).get();
+		return findUser;
+	}
 	
 }
