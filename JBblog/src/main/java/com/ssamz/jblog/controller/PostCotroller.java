20f510bb4e86eafc145fa692607f21def132d992
dogby <dogby@DESKTOP-407VL6U>
2023-04-03 18:52:56 +0900

    2023-04-03 : w3school navbar

--- JBblog/src/main/java/com/ssamz/jblog/controller/PostCotroller.java
+++ JBblog/src/main/java/com/ssamz/jblog/controller/PostCotroller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostCotroller {
-	@GetMapping({})
+	@GetMapping({"","/"})
 		public String getPostList(){
 			return "index";
 		}	
